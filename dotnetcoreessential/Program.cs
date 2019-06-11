9d51f3d203478014e400b6661763207a219adedd
Rajaraman <Rajaraman@DESKTOP-E20IQQL>
2019-06-11 15:53:04 +0530

    Add project files.

---
+++ dotnetcoreessential/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcoreessential
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
